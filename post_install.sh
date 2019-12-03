7b8d4519b5833c596b62d8f60b8f6bed70c4e4d4
RogerAirgood <rogerairgood@gmail.com>
2019-12-02 19:39:59 -0800

    Add files via upload

--- post_install.sh
+++ post_install.sh
@@ -5,8 +5,7 @@ sysrc -f /etc/rc.conf php_fpm_enable="YES"
 service php-fpm start 2>/dev/null
 service lighttpd start 2>/dev/null
 
-mkdir /root/.autodl
-touch /root/.autodl/autodl.cfg
+
 echo "[options]" >> /root/.autodl/autodl.cfg
 echo "rt-address = 127.0.0.1:6000" >> /root/.autodl/autodl.cfg
 echo "gui-server-port = 6002" >> /root/.autodl/autodl.cfg
@@ -18,13 +17,13 @@ rm -r rutorrent/plugins
 svn checkout https://github.com/Novik/ruTorrent/trunk/plugins rutorrent/plugins
 
 
+
 rm -rf plugins/autodl-irssi
 
 git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi 
 
 
-
-# (crontab -l 2>/dev/null; echo "@reboot sh /root/torrenting.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sh /root/torrenting.sh") | crontab -
 
 screen -dmS screen_rtorrent rtorrent
 screen -S autodl -fa -d -m irssi
