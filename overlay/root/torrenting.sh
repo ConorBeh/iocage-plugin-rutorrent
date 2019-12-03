6ce21428ede6d2d7351d1d8f99c78092a485fb53
RogerAirgood <rogerairgood@gmail.com>
2019-12-03 15:24:59 -0800

    Delete torrenting.sh

--- overlay/root/torrenting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
-export TERM=xterm
-# Start rtorrent in detached screen
-screen -dmS screen_rtorrent rtorrent
-screen -S autodl -fa -d -m irssi
