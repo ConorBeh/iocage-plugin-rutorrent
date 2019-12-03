abc309701a2f1609eccf1ed1e6877083e7761916
RogerAirgood <rogerairgood@gmail.com>
2019-12-02 22:40:14 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,18 +12,23 @@ echo "gui-server-port = 6002" >> /root/.autodl/autodl.cfg
 echo "gui-server-password = 3PicP4ssw0rd" >> /root/.autodl/autodl.cfg
 
 mkdir /root/rutorrent-stage
+mkdir /root/rutorrent-stage-2
+mkdir /roo/rutorrent-stage-3
+
 git clone https://github.com/Novik/ruTorrent.git rutorrent-stage
-rm -r rutorrent-stage/plugins
-svn checkout https://github.com/Novik/ruTorrent/trunk/plugins rutorrent-stage-3/plugins
-cp -rf rutorrent-stage-3/plugins. /usr/local/www/rutorrent/plugins/
-
-
-rm -r rutorrent-stage/plugins/autodl-irssi
-
-git clone https://github.com/autodl-community/autodl-rutorrent.git rutorrent-stage-2/plugins/autodl-irssi 
-
+rm -r rutorrent-stage/plugins/
 cp -rf rutorrent-stage/. /usr/local/www/rutorrent/
-cp -rf rutorrent-stage-2/. /usr/local/www/rutorrent/
+
+svn checkout https://github.com/Novik/ruTorrent/trunk/plugins rutorrent-stage-3/plugins
+cp -rf rutorrent-stage-3/plugins /usr/local/www/rutorrent/
+
+rm -r /usr/local/www/rutorrent/autodl-irssi
+
+
+
+git clone https://github.com/autodl-community/autodl-rutorrent.git rutorrent-stage-2
+cp -rf rutorrent-stage-2/autodl-irssi /usr/local/www/rutorrent/plugins/
+
 
 mkdir config-stage
 git clone https://github.com/RogerAirgood/iocage-plugin-rutorrent.git config-stage
