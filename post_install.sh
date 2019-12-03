94d321ae349227f0a007f35353889a1aa72a3604
RogerAirgood <rogerairgood@gmail.com>
2019-12-02 21:21:26 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -11,17 +11,19 @@ echo "rt-address = 127.0.0.1:6000" >> /root/.autodl/autodl.cfg
 echo "gui-server-port = 6002" >> /root/.autodl/autodl.cfg
 echo "gui-server-password = 3PicP4ssw0rd" >> /root/.autodl/autodl.cfg
 
-cd /usr/local/www/
-git clone https://github.com/Novik/ruTorrent.git rutorrent
-rm -r rutorrent/plugins
-svn checkout https://github.com/Novik/ruTorrent/trunk/plugins rutorrent/plugins
+mkdir /root/rutorrent-stage
+git clone https://github.com/Novik/ruTorrent.git rutorrent-stage
+rm -r rutorrent-stage/plugins
+svn checkout https://github.com/Novik/ruTorrent/trunk/plugins rutorrent-stage/plugins
 
 
 
-rm -rf rutorrent/plugins/autodl-irssi
+rm -r rutorrent-stage/plugins/autodl-irssi
 
-git clone https://github.com/autodl-community/autodl-rutorrent.git rutorrent/plugins/autodl-irssi 
+git clone https://github.com/autodl-community/autodl-rutorrent.git rutorrent-stage-2/plugins/autodl-irssi 
 
+cp -rf rutorrent-stage/. /usr/local/www/rutorrent/
+cp -rf rutorrent-stage-2/. /usr/local/www/rutorrent/
 
 (crontab -l 2>/dev/null; echo "@reboot sh /root/torrenting.sh") | crontab -
 
